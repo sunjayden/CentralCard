d9b7a841eda761454e96858142ce92f9be40b418
Jayden <jayden_sun@outlook.com>
2016-10-22 20:00:35 -0400

    added log on page

--- app/src/main/java/me/jaydensun/android/centralcard/MainActivity.java
+++ app/src/main/java/me/jaydensun/android/centralcard/MainActivity.java
@@ -1,7 +1,11 @@
 package me.jaydensun.android.centralcard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button logOn = (Button)findViewById(R.id.log_on_button);
+        logOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent numberIntent = new Intent(MainActivity.this, LogOnActivity.class);
+                startActivity(numberIntent);
+            }
+        });
     }
 }
