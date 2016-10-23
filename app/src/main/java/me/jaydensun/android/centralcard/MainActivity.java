f17cc7a7d2101c8ccd1b68dbb75f9bb65b58d3ab
Jayden <jayden_sun@outlook.com>
2016-10-22 21:30:10 -0400

    added sign in and sign up page

--- app/src/main/java/me/jaydensun/android/centralcard/MainActivity.java
+++ app/src/main/java/me/jaydensun/android/centralcard/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +18,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent numberIntent = new Intent(MainActivity.this, LogOnActivity.class);
+                startActivity(numberIntent);
+            }
+        });
+
+
+        Button signUp = (Button)findViewById(R.id.sign_up_button);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent numberIntent = new Intent(MainActivity.this, SignUpActivity.class);
                 startActivity(numberIntent);
             }
         });
